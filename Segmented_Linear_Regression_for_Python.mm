28327440a0caae5c958a10eb9978df7e7181a860
Andrey Pohilko <undera@yandex-team.ru>
2014-02-17 16:17:06 +0400

    FreeMind doc with thoughts

---
+++ Segmented_Linear_Regression_for_Python.mm
@@ -0,0 +1,41 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1392639077553" ID="ID_1654421549" MODIFIED="1392639238241" TEXT="Segmented Linear Regression for Python">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1392639131834" ID="ID_715201506" MODIFIED="1392639140583" POSITION="right" TEXT="use numpy/scipy">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1392639141344" ID="ID_979674620" MODIFIED="1392639152333" TEXT="http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html#scipy.stats.linregress">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1392639220072" ID="ID_81317591" MODIFIED="1392639305216" POSITION="right" TEXT="Single-Pass Algo">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1392639243880" ID="ID_1130284408" MODIFIED="1392639254616" TEXT="initial seed">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1392639255264" ID="ID_1662376660" MODIFIED="1392639273097" TEXT="to find global optimum">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1392639277256" ID="ID_192307653" MODIFIED="1392639288961" TEXT="seed candidate count">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1392639352232" ID="ID_247089753" MODIFIED="1392639365679" TEXT="BP movement">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1392639306288" ID="ID_1337460765" MODIFIED="1392639314928" POSITION="right" TEXT="Multi-Pass algo">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1392639332560" ID="ID_1187281350" MODIFIED="1392639342230" TEXT="criteria to stop?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
